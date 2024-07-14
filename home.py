563dd3efb9fedb789f98e1318ec17af13f8c4548
DEAN <ssgardaf@gmail.com>
2024-07-14 17:33:11 +0900

    완성 2

--- home.py
+++ home.py
@@ -3,7 +3,7 @@ import json
 import streamlit as st
 st.set_page_config(layout="wide")  # 와이드 화면 설정
 
-st.title("건강 헬퍼")
+st.title("웰빙 매니저")
 
 def get_health_info(glucose, cholesterol, ldl, height, weight, sex, systolic_bp, diastolic_bp, heart_rate, hdl, bmi, alt, ast, uric_acid):
     headers = {
